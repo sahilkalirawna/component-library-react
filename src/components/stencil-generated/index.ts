6d731db6b54e392dd942ef090b5beff829a986ee
Sahil kalirawna <sahilkalirawna@gmail.com>
2022-07-25 18:18:31 +0530

    initial wrapper

---
+++ src/components/stencil-generated/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'component-library';
+
+import { defineCustomElements } from 'component-library/loader';
+
+defineCustomElements();
+export const FlipCard = /*@__PURE__*/createReactComponent<JSX.FlipCard, HTMLFlipCardElement>('flip-card');
+export const MyAccordion = /*@__PURE__*/createReactComponent<JSX.MyAccordion, HTMLMyAccordionElement>('my-accordion');
